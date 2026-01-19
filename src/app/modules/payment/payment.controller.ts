eccf3942fa7348c8be3c985f98e83c72ee7e5cbc
Ahmedturaj <ahmedturaj12@gmail.com>
2026-01-20 01:20:54 +0600

    remove some comment code

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -6,44 +6,6 @@ import User from '../user/user.model';
 import sendResponse from '../../utils/sendResponse';
 import AppError from '../../error/appError';
 
-// export const createPayment = async (req: Request, res: Response) => {
-//   const { productId } = req.body;
-//   const userId = req.user.id; 
-
-//   if (!productId) {
-//     return res.status(400).json({ message: 'productId is required' });
-//   }
-
-//   const product = await Product.findById(productId);
-//   if (!product) {
-//     return res.status(404).json({ message: 'Product not found' });
-//   }
-
-//   const amount = product.price;
-
-//   const paymentIntent = await stripe.paymentIntents.create({
-//     amount: Math.round(amount * 100),
-//     currency: 'usd',
-//     metadata: {
-//       userId,
-//       productId,
-//     },
-//   });
-
-//   await Payment.create({
-//     userId,
-//     productId,
-//     amount,
-//     transactionId: paymentIntent.id,
-//     status: 'pending',
-//   });
-
-//   res.status(200).json({
-//     success: true,
-//     clientSecret: paymentIntent.client_secret,
-//   });
-// };
-
 export const createPayment = async (req: Request, res: Response) => {
   const { productId } = req.body;
   const userId = req.user.id;
@@ -106,30 +68,6 @@ export const createPayment = async (req: Request, res: Response) => {
     },
   });
 };
-
-
-// export const confirmPayment = async (req: Request, res: Response) => {
-//   const { paymentIntentId } = req.body;
-
-//   const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
-
-//   if (paymentIntent.status === 'succeeded') {
-//     await Payment.findOneAndUpdate(
-//       { transactionId: paymentIntentId },
-//       { status: 'success' }
-//     );
-
-//     return res.json({ success: true });
-//   }
-
-//   await Payment.findOneAndUpdate(
-//     { transactionId: paymentIntentId },
-//     { status: 'failed' }
-//   );
-
-//   res.status(400).json({ success: false });
-// };
-
 
 export const confirmPayment = async (req: Request, res: Response) => {
   const { paymentIntentId } = req.body;
